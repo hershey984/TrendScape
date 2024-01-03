38820bae74c310a0b5ad56d053e50b8551a2e7e4
hershey984 <155549232+hershey984@users.noreply.github.com>
2024-01-03 18:51:56 +0530

    Add files via upload

---
+++ indscript.js
@@ -0,0 +1,25 @@
+function home(){
+    window.location.href="sub.html";
+
+}
+
+function about(){
+
+}
+   
+function dropcategories(){
+    var dropdownContent = document.querySelector('.categoriesbutton .dropdowncontent');
+    dropdownContent.style.display = (dropdownContent.style.display === 'block') ? 'none' : 'block';
+}
+
+function explore(){
+
+}
+
+function contact(){
+
+}
+
+function login(){
+
+}
